552c218a3669bd882870f1657f4c43042e4f70dd
msandholz <markus.sandholz@t-online.de>
2022-11-17 10:56:09 +0100

    Update config.ino

--- config.ino
+++ config.ino
@@ -34,3 +34,42 @@ void loadConfig() {
 
     config_file.close();
 }
+
+
+void saveConfig() {
+   
+    if(!SPIFFS.begin(true)){
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+    }
+     // Open file for writing
+   File config_file = SPIFFS.open("/config.json", FILE_WRITE);
+
+  if (!config_file) {
+    Serial.println(F("Failed to create file"));
+    return;
+  }
+
+  // Allocate a temporary JsonDocument
+  // Don't forget to change the capacity to match your requirements.
+  // Use arduinojson.org/assistant to compute the capacity.
+  StaticJsonDocument<256> doc;
+
+doc["HOSTNAME"] = "John";
+doc["WIFI_SSID"] = "WLAN";
+doc["WIFI_PW"] = "xyz";
+doc["MQTT_SERVER"] = "";
+doc["TARGET_TEMP"] = 10;
+doc["TEMP_HYSTERESIS"] = 1;
+doc["POWER_HEAT"] = 120;
+doc["POWER_COOL"] = 240;
+doc["POWER_IDLE"] = 10;
+
+  // Serialize JSON to file
+  if (serializeJson(doc, config_file) == 0) {
+    Serial.println(F("Failed to write to file"));
+  }
+
+  // Close the file
+  config_file.close();
+}
